d80e203780fd319dfb6ca9d4267b4d26c1760d1c
AugustoMagalhaes <augustommg@gmail.com>
2022-03-11 16:13:01 -0300

    R01: Implementando o modulo de acesso a api do Mercado Livre em pair-programming com todo o time.

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,16 @@
 export async function getCategories() {
   // Implemente aqui
+  const data = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const categories = await data.json();
+  console.log('categories: ', categories);
+  return categories;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const fetchData = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+  const categoriesAndQuery = await fetchData.json();
+  console.log('catQuery ', categoriesAndQuery);
+  return categoriesAndQuery;
+  /* const filter = categories.filter((el) => el.name.includes(produto) || el.id.includes(umId));
+  console.log('fil', filter); */
 }
